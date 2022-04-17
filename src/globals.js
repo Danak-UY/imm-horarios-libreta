8fc65387e6ce253c79382224ab313182120ff171
Danak-UY <m.claderaa@gmail.com>
2022-04-17 11:48:18 -0300

    feat(): get available days per month

--- src/globals.js
+++ src/globals.js
@@ -2,4 +2,9 @@ module.exports = {
   URL: "https://www.montevideo.gub.uy/sae/agendarReserva/Paso1.xhtml?agenda=RENOVCON&recurso=RENCOMUN&solo_cuerpo=true",
   EXCLUDED_RESOURCES: ["image", "stylesheet", "media", "font", "other"],
   AVAILABLE_CLASS: "diaSinCupo",
+  BOUNDARY_MONTH: "rich-calendar-boundary-date",
+  CURRENT_MONTH: "rich-calendar-month",
+  CALENDAR_TOOL: "rich-calendar-tool",
+  CALENDAR_BUTTON: "rich-calendar-tool-btn",
+  POST_REQUEST: "https://www.montevideo.gub.uy/sae/agendarReserva/Paso1.xhtml",
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,37 @@
 (async () => {
   const { chromium } = require("playwright");
-  const { URL, EXCLUDED_RESOURCES, AVAILABLE_CLASS } = require("./globals.js");
+  const {
+    URL,
+    EXCLUDED_RESOURCES,
+    AVAILABLE_CLASS,
+    BOUNDARY_MONTH,
+    CURRENT_MONTH,
+    CALENDAR_TOOL,
+    CALENDAR_BUTTON,
+    POST_REQUEST,
+  } = require("./globals.js");
+
+  const getAvailableDays = async () => {
+    const availableDays = await page.$$(
+      `.${AVAILABLE_CLASS}:not(.${BOUNDARY_MONTH})`
+    );
+    return Promise.all(availableDays.map(async (day) => day.innerText()));
+  };
+
+  const getCurrentMonth = async () => {
+    const $currentMonth = await page.$(`.${CURRENT_MONTH}`);
+    const $currentMonthText = await $currentMonth.innerText();
+    const [currentMonth] = $currentMonthText.split(",");
+    return currentMonth;
+  };
+
+  const getMonthData = async () => {
+    const currentMonth = await getCurrentMonth();
+    const availableDays = await getAvailableDays();
+    return {
+      [currentMonth]: availableDays,
+    };
+  };
 
   // Create new chromium browser
   const browser = await chromium.launch();
@@ -22,8 +53,37 @@
   await page.goto(URL);
 
   // Get all available days
-  const availableDays = await page.$$(`.${AVAILABLE_CLASS}`);
-  console.log("Available", availableDays.length);
+  const currentMonthData = await getMonthData();
+  console.log(currentMonthData);
+
+  // Get next month button
+  const [, , $nextMonthBtn] = await page.$$(
+    `.${CALENDAR_TOOL} > .${CALENDAR_BUTTON}`
+  );
+
+  // Wait for next month response
+  const [response] = await Promise.all([
+    // Waits for the next response matching some conditions
+    page.waitForResponse(
+      (response) =>
+        response.url().startsWith(POST_REQUEST) && response.status() === 200
+    ),
+    // Triggers the response
+    $nextMonthBtn.click(),
+  ]);
+
+  const nextMonthData = await getMonthData();
+  console.log(nextMonthData);
+
+  const availableDays = [
+    ...Object.values(currentMonthData),
+    ...Object.values(nextMonthData),
+  ].flat();
+  const hasAvailableDays = availableDays.length > 0;
+
+  if (hasAvailableDays) {
+    console.log(availableDays);
+  }
 
   //Take screenshot and save it
   await page.screenshot({ path: `example.png` });
